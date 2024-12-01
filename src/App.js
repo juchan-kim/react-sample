2044db1e75c2cc144f334f4f30bba1aa0963d55c
juchan-kim <113649125+juchan-kim@users.noreply.github.com>
2024-12-01 13:37:13 +0900

    edit App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    )
+  }
+}
+
+class TOC extends Component {
+  render() {
+    
+    const list = [];
+    let i = 0;
+    while (i < this.props.data.length) {
+      const data = this.props.data[i];
+      list.push(
+      <li key={data.id}>
+        <a href={data.id + '.html'} onClick={function(id, ev) {
+          ev.preventDefault()
+          this.props.onSelect(id);
+        }.bind(this, data.id)}>{data.title}          
+        </a>
+      </li>
+      );
+      i++;
+    }
+     
+    /*
+    const list = this.props.data.map(el => (<li><a href="1.html">{el.title}</a></li>));  
+    */
+    return (
+      <nav>
+        <ol>
+            {list}
+        </ol>
+      </nav>
+    )
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+    <article>
+      <h2>{this.props.data.title}</h2>
+      {this.props.data.desc}
+    </article>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    selected_content_id: 2,
+    contents: [
+      {id: 1, title: 'HTML', desc: 'HTML is for information'},
+      {id: 2, title: 'CSS', desc: 'CSS is for style'},
+      {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactiono'}
+    ]
+  }
+
+  getSelectedContent() {
+    let i = 0;
+    while(i < this.state.contents.length) {
+      const data = this.state.contents[i]
+      if(this.state.selected_content_id === data.id) {
+        return data;
+      }
+      i = i + 1;
+    }
+  }
+
+  render() {
+    let content = this.getSelectedContent();
+    console.log(content);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Subject title="WEB" sub="World wide web"></Subject>
+      <TOC onSelect={function(id) {
+        console.log('app', id);
+        this.setState({
+          selected_content_id: id
+      })
+      }.bind(this)}  data={this.state.contents}></TOC>
+      <Content data={this.getSelectedContent()}></Content>
     </div>
-  );
+  )
+}
 }
 
 export default App;
